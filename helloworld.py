64def9091263cc2f53e413894f6e9f3827c1f47a
Felix Montanari <captainboggle108@gmail.com>
2021-06-04 13:31:11 +1000

    added tower class

--- helloworld.py
+++ helloworld.py
@@ -10,8 +10,24 @@ screen = pygame.display.set_mode((900,580))
 bg1 = pygame.image.load(os.path.join("mapideas", "hedge.png"))
 bg2 = pygame.image.load(os.path.join("mapideas", "meadow.png"))
 bg3 = pygame.image.load(os.path.join("mapideas", "rake.png"))
+
+ts1 = pygame.image.load(os.path.join("towers", "drafts","t1.png")).convert_alpha()
+ts2 = pygame.image.load(os.path.join("towers", "drafts","t1o.png")).convert_alpha()
+ts3 = pygame.image.load(os.path.join("towers", "drafts","t2.png")).convert_alpha()
+ts4 = pygame.image.load(os.path.join("towers", "drafts","t2o.png")).convert_alpha()
 bg = bg1
+
+class Tower(pygame.sprite.Sprite):
+    def __init__(self,image,pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect(center=pos)
+
+
+        
+dragging = False
 while True:
+    mx, my = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
