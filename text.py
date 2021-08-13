8223279f0614352efcbed8aaaa1a05edace569ce
Anna Kuchina <annakkuchina@gmail.com>
2021-08-13 14:45:28 +1000

    added a font

--- text.py
+++ text.py
@@ -5,12 +5,16 @@ screen = globs.SCREEN
 SCREEN_WIDTH, SCREEN_HEIGHT = pygame.display.get_surface().get_size()
 
 pygame.font.init()
+
+
 SMALL_FONT = pygame.font.Font("retro.ttf", 25)
 FONT = pygame.font.Font("retro.ttf", 35)
 MEDIUM_FONT = pygame.font.Font("retro.ttf", 50)
 LARGE_FONT = pygame.font.Font("retro.ttf", 80)
 CONTRAST_MEDIUM_FONT = pygame.font.Font("retro.ttf", 51)
 CONTRAST_LARGE_FONT = pygame.font.Font("retro.ttf", 83)
+
+INFO_FONT = pygame.font.Font("data-latin.ttf", 355)
 
 
 def draw_text(text, font, color, surface, x, y):
